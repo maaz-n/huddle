6800a07a407c34c755e2d0125ee2ab02d9c2536d
Maaz <justmaazkhan@gmail.com>
2026-01-08 12:23:25 +0500

    added cookie config

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,16 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true
+    },
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
 
-    emailAndPassword: {
-        enabled: true
-    }
+
+    plugins: [nextCookies(  )]
 });
