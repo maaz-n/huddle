3fe145d70a138a86082a4974d9ce87ef0c81346f
Maaz <justmaazkhan@gmail.com>
2026-01-08 17:23:49 +0500

    added custom user type

---
+++ types/types.ts
@@ -0,0 +1,9 @@
+export type UserType = {
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    email: string;
+    emailVerified: boolean;
+    name: string;
+    image?: string | null | undefined;
+} | undefined
