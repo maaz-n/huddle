7843244b259aca7d35a73ab4e34edf7446478e68
Maaz <justmaazkhan@gmail.com>
2026-01-10 10:39:58 +0500

    modified create function - now creates membership

--- actions/workspace.ts
+++ actions/workspace.ts
@@ -1,15 +1,16 @@
 "use server"
 
 import { db } from "@/db"
-import { workspaces } from "@/db/schema"
+import { workspaceMembers, workspaces } from "@/db/schema"
 import { getCurrentUser } from "./auth"
 
 export const createWorkspace = async (name: string) => {
     try {
         const user = await getCurrentUser();
         if(!user) throw("User not logged in!")
-        await db.insert(workspaces).values({name: name, ownerId: user.id});
-        return {success: true, message: "Workspace crated!"}
+        const workspace = await db.insert(workspaces).values({name: name, ownerId: user.id}).returning();
+        await db.insert(workspaceMembers).values({workspaceId: workspace[0].id, userId: user.id, role: "admin"})
+        return {success: true, message: "Workspace created!"}
     } catch (error) {
         console.error(error)
         return {success: false, message: "There was an error creating the workspace"}
