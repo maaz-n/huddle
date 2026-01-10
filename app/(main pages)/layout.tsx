a0e737362e5fbd4fa7044c36e5955337e880ab03
Maaz <justmaazkhan@gmail.com>
2026-01-10 10:43:15 +0500

    created a main page layout

---
+++ app/(main pages)/layout.tsx
@@ -0,0 +1,19 @@
+import { getCurrentUser } from '@/actions/auth';
+import { getWorkspaces } from '@/actions/tasks'
+import { AppDataProvider } from '@/components/app-data-provider';
+import React from 'react'
+
+const MainPagesLayout = async ({children} : {children: React.ReactNode}) => {
+
+    const workspaces = await getWorkspaces();
+    const user = await getCurrentUser();
+    if(!user) throw Error("User not logged in!")
+
+  return (
+    <AppDataProvider user={user} workspaces={workspaces}>
+        {children}
+    </AppDataProvider>
+  )
+}
+
+export default MainPagesLayout
