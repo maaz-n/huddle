0e842b864369af18d84f3acc28793a382b876b1b
Maaz <justmaazkhan@gmail.com>
2026-02-05 22:58:06 +0500

    fixed sidebar auto opening on new pages in phones

--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -11,14 +11,12 @@ interface AppLayoutProps {
 }
 
 export function AppLayout({ children }: AppLayoutProps) {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className="flex h-screen bg-background">
-      {/* Sidebar */}
       <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
 
-      {/* Main content */}
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main className="flex-1 overflow-y-auto">
