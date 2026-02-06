65a05721c3ce0460ee104dc449d7b859c65de327
Maaz <justmaazkhan@gmail.com>
2026-02-06 22:14:41 +0500

    added google oauth login

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,13 +4,19 @@ import { db } from "@/db";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
     emailAndPassword: {
         enabled: true
     },
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
 
-
-    plugins: [nextCookies(  )]
+    plugins: [nextCookies()]
 });
